e23bb8f65cff8b2b681b394f4a0c3b3987d26140
unknown <sanvicola2000@gmail.com>
2021-03-16 14:16:14 +0100

    Added basic pages and setup routing config

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,51 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/contact">Contact</router-link> |
-    <router-link to="/portfolio">Portfolio</router-link> 
+    <router-link to="/portfolio">Portfolio</router-link>
   </div>
-  <router-view/>
+  <router-view v-slot="slotProps">
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  overflow-x: hidden;
+  height: 100%;
+  scroll-behavior: smooth;
+}
+
+body {
+  font-family: "Poppins", sans-serif;
+  font-size: 15px;
+  color: #fff;
+  font-weight: 500;
+  line-height: 1.6;
+  position: relative;
+  background-size: cover;
+  background-position: center;
+  height: 100%;
+  width: 100%;
+  background-color: #111;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.poppins-font {
+  font-family: "Poppins", sans-serif;
+}
+
+.open-sans-font {
+  font-family: "Open Sans", sans-serif;
 }
 
 #nav {
@@ -22,11 +55,26 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #ffffff;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #6957af;
     }
   }
+}
+
+.route-enter-from,
+.route-leave-to{
+  opacity: 0;
+}
+
+.route-enter-active,
+.route-leave-active {
+  transition: opacity 0.2s linear;
+}
+
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
 }
 </style>
